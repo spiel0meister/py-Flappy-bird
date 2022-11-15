3f1abe36f732e17ac929c7e7c2f1c9a155738be4
spiel0meister <soviczan7@gmail.com>
2022-11-15 20:53:43 +0100

    Added assets and rendering background.

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
+import pygame
+
+pygame.init()
+WIDTH, HEIGHT = 474, 355
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
+bg = pygame.image.load("assets/background.bmp")
+
+
+def draw():
+    WIN.blit(bg, bg.get_rect())
+
+
 def main():
-    pass
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+
+        draw()
+        clock.tick(30)
+        pygame.display.update()
 
 
 if __name__ == "__main__":
