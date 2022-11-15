836d5028f2ced1badd4c3430f70314c9878bd597
spiel0meister <soviczan7@gmail.com>
2022-11-15 21:49:39 +0100

    Added pipes
    Added generating pipes and drawing them.

--- Pipe.py
+++ Pipe.py
@@ -1,2 +1,16 @@
 class Pipe:
-    pass
+    def __init__(self, x, y, gap, img, rotated_img):
+        self.x = x
+        self.y = y
+        self.gap = gap
+        self.img_a = img
+        self.img_b = rotated_img
+
+    def update(self):
+        self.x -= 1
+
+    def draw(self, surface):
+        surface.blit(self.img_a, (self.x, self.y,
+                     self.img_a.get_width(), self.img_a.get_height()))
+        surface.blit(self.img_b, (self.x, self.y + self.img_b.get_height() + self.gap,
+                     self.img_b.get_width(), self.img_b.get_height()))
--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 import pygame
+import Pipe
+import Bird
+from random import random
 
 pygame.init()
 WIDTH, HEIGHT = 474, 355
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 bg = pygame.image.load("assets/background.bmp")
+pipe_img = pygame.image.load("assets/pipe.bmp")
+rotated_pipe_img = pygame.transform.rotate(pipe_img, 180)
+bird_img = pygame.image.load("assets/bird.bmp")
 
 
-def draw():
+pipes = []
+
+
+def draw(WIN):
     WIN.blit(bg, bg.get_rect())
+    for pipe in pipes:
+        pipe.draw(WIN)
 
 
 def main():
+    frame_count = 0
+
     run = True
     while run:
         for event in pygame.event.get():
@@ -19,9 +32,19 @@ def main():
                 run = False
                 break
 
-        draw()
-        clock.tick(30)
+        peace = random()
+        while peace <= 0.1 or peace >= 0.9:
+            peace = random()
+
+        for pipe in pipes:
+            pipe.update()
+        draw(WIN)
+        if frame_count % (pipe_img.get_width() * 3) == 0:
+            pipes.append(Pipe.Pipe(WIDTH, -rotated_pipe_img.get_height() * (1 - peace), HEIGHT/4,
+                                   rotated_pipe_img,  pipe_img))
         pygame.display.update()
+        frame_count += 1
+        clock.tick(30)
 
 
 if __name__ == "__main__":
